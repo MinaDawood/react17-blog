efd17ff84830d00ba1cc270fcc8f7e69dbb9a937
minadawood <mina.ziden@hotmail.com>
2021-01-21 09:54:27 +0200

    Add Routes

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,13 @@
-import { useState, useEffect } from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-      });
-  }, []);
-
+  const { data: blogs, isPending, error } = useFetch(
+    'http://localhost:8000/blogs'
+  );
   return (
     <div className="home">
+      {error && <div>{error} </div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
